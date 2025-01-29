f4dbc6e7ad445973b51ae6fbaf31d378dbfb92fd
Marina Joy <marina14kodai@gmail.com>
2025-01-29 16:32:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chocolates" element={<Chocolates addToCart={addToCart} />} />
-        <Route path="/other-items" element={<OtherItems addToCart={addToCart}/>} />
+        <Route path="/other-items" element={<OtherItems addToCart={addToCart} updateQuantity={updateQuantity} removeFromCart={removeFromCart}/>} />
         <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity} removeFromCart={removeFromCart} />} /> 
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
