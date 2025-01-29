d4a6ad6632e9e526066d8b80051b68fa1960ff9e
Marina Joy <marina14kodai@gmail.com>
2025-01-29 16:37:02 +0530

    Update chocolates.js

--- src/components/chocolates.js
+++ src/components/chocolates.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const chocolates = [
-  { id: 1, name: "Almond Rich", price: "$10", image: "/JSR-Natural-s-chocolates/c1.jpg", category: "Dark" },
+  { id: 1, name: "Almond Rich", price: "$10", image: "/c1.jpg", category: "Dark" },
   { id: 2, name: "White Rich", price: "$8", image: "/c2.jpg", category: "White" },
   { id: 3, name: "Milk Crunch", price: "$9", image: "/c3.jpg", category: "Milk" },
   { id: 4, name: "Roasted Cashew", price: "$10", image: "/c4.jpg", category: "Milk" },
